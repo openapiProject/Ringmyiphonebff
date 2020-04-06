7d2a07404efdfe9a728154b9210047cbaee3b24d
nick chang <nickchang244@yahoo.com>
2020-04-05 23:57:07 -0500

    init repo

--- src/main/java/com/openapisession/ringmyiphonebff/FireBaseService.java
+++ src/main/java/com/openapisession/ringmyiphonebff/FireBaseService.java
@@ -5,15 +5,16 @@ import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.cloud.FirestoreClient;
+import com.openapisession.ringmyiphonebffapi.model.User;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ExecutionException;
 
 @Service
 public class FireBaseService {
-    public User getUserDetails(String name) throws InterruptedException, ExecutionException {
+    public User getUserDetails(String userId) throws InterruptedException, ExecutionException {
         Firestore dbFirestore = FirestoreClient.getFirestore();
-        DocumentReference documentReference = dbFirestore.collection("users").document(name);
+        DocumentReference documentReference = dbFirestore.collection("users").document(userId);
         ApiFuture<DocumentSnapshot> future = documentReference.get();
 
         DocumentSnapshot document = future.get();
--- src/main/java/com/openapisession/ringmyiphonebff/firebaseUserController.java
+++ src/main/java/com/openapisession/ringmyiphonebff/firebaseUserController.java
@@ -1,19 +1,27 @@
 package com.openapisession.ringmyiphonebff;
 
+import com.openapisession.ringmyiphonebffapi.api.GetuserApi;
+import com.openapisession.ringmyiphonebffapi.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class firebaseUserController implements FireBaseUserApi {
+public class firebaseUserController implements GetuserApi {
 
     @Autowired FireBaseService fireBaseService;
 
     @Override
-    public ResponseEntity<User> getUser(@PathVariable("name") String name)
+            public ResponseEntity<User> getuser( @PathVariable("userId") String userId,
+                                         @RequestHeader(value="Accept-Version", required=false) String acceptVersion,
+                                         @RequestHeader(value="X-B3-SpanId", required=false) String xB3SpanId,
+                                        @RequestHeader(value="X-B3-TraceId", required=false) String xB3TraceId,
+                                         @RequestHeader(value="Authorization", required=false) String authorization)
         throws Exception {
-        return ResponseEntity.ok().body(fireBaseService.getUserDetails(name));
+        return ResponseEntity.ok().body(fireBaseService.getUserDetails(userId));
     }
 
 }
